728cb5596f5e8d0b7b9a380241436b3a686510c7
Avi Romanoff <avi@romanoff.me>
2016-11-23 16:35:30 -0500

    Refactor, add custom font

--- app/src/main/java/avi/bio/huesaturationvictory/FullscreenActivity.java
+++ app/src/main/java/avi/bio/huesaturationvictory/FullscreenActivity.java
@@ -1,87 +1,27 @@
 package avi.bio.huesaturationvictory;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class FullscreenActivity extends AppCompatActivity implements SensorEventListener {
+public class FullscreenActivity extends AppCompatActivity {
+
     private SensorManager mSensorManager;
     private GameSurfaceView mSurfaceView;
-
-    // Create a constant to convert nanoseconds to seconds.
-    private static final float NS2S = 1.0f / 1000000000.0f;
-    private float[] deltaRotationVector = new float[4];
-    private float timestamp;
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        // This timestep's delta rotation to be multiplied by the current rotation
-        // after computing it from the gyro sample data.
-        if (timestamp != 0) {
-            final float dT = (event.timestamp - timestamp) * NS2S;
-            // Axis of the rotation sample, not normalized yet.
-            float axisX = event.values[0];
-            float axisY = event.values[1];
-            float axisZ = event.values[2];
-
-            // Calculate the angular speed of the sample
-            float omegaMagnitude = (float) Math.sqrt(Math.ceil(axisX*axisX + axisY*axisY + axisZ*axisZ));
-
-            // Normalize the rotation vector if it's big enough to get the axis
-            // (that is, EPSILON should represent your maximum allowable margin of error)
-            if (omegaMagnitude > 0.000001f) {
-                axisX /= omegaMagnitude;
-                axisY /= omegaMagnitude;
-                axisZ /= omegaMagnitude;
-            }
-
-            // Integrate around this axis with the angular speed by the timestep
-            // in order to get a delta rotation from this sample over the timestep
-            // We will convert this axis-angle representation of the delta rotation
-            // into a quaternion before turning it into the rotation matrix.
-            float thetaOverTwo = omegaMagnitude * dT / 2.0f;
-            float sinThetaOverTwo = (float)Math.sin(Math.ceil(thetaOverTwo));
-            float cosThetaOverTwo = (float)Math.cos(Math.ceil(thetaOverTwo));
-            deltaRotationVector[0] = sinThetaOverTwo * axisX;
-            deltaRotationVector[1] = sinThetaOverTwo * axisY;
-            deltaRotationVector[2] = sinThetaOverTwo * axisZ;
-            deltaRotationVector[3] = cosThetaOverTwo;
-        }
-        timestamp = event.timestamp;
-        float[] deltaRotationMatrix = new float[9];
-        SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
-        // User code should concatenate the delta rotation we computed with the current rotation
-        // in order to get the updated rotation.
-        // rotationCurrent = rotationCurrent * deltaRotationMatrix;
-        mSurfaceView.setRotationVector(deltaRotationVector);
-    }
-
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // TODO Auto-generated method stub
-    }
+    private SensorEngine mSensorEngine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("HSV", "onCreate()");
 
         setContentView(R.layout.activity_fullscreen);
 
@@ -92,11 +32,12 @@ public class FullscreenActivity extends AppCompatActivity implements SensorEvent
         }
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensorManager.registerListener(
-                this,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY),
-                SensorManager.SENSOR_DELAY_GAME
-        );
+        mSensorEngine = new SensorEngine(new SensorChangedListener() {
+            @Override
+            public void onSensorChanged(float[] rotationVector) {
+                mSurfaceView.setRotationVector(rotationVector);
+            }
+        });
     }
 
     @Override
@@ -106,21 +47,42 @@ public class FullscreenActivity extends AppCompatActivity implements SensorEvent
     }
 
     @Override
+    protected void onStart() {
+        Log.d("HSV", "onStart()");
+        super.onResume();
+
+        mSensorManager.registerListener(
+                mSensorEngine,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY),
+                SensorManager.SENSOR_DELAY_GAME
+        );
+    }
+
+    @Override
     protected void onResume() {
+        Log.d("HSV", "onResume()");
         super.onResume();
 
         // Tell the gameView resume method to execute
         mSurfaceView.resume();
+
         hide();
     }
 
     // This method executes when the player quits the game
     @Override
     protected void onPause() {
+        Log.d("HSV", "onPause()");
         super.onPause();
 
+        // TODO: Not working
         // Tell the gameView pause method to execute
-        mSurfaceView.pause();
+//        mSurfaceView.pause();
+
+        mSensorManager.unregisterListener(
+                mSensorEngine,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
+        );
     }
 
     private void hide() {
--- app/src/main/java/avi/bio/huesaturationvictory/GameSurfaceView.java
+++ app/src/main/java/avi/bio/huesaturationvictory/GameSurfaceView.java
@@ -4,12 +4,16 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.SystemClock;
 import android.os.Vibrator;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import static android.util.Log.d;
 
 /**
  * Created by avi on 11/19/16.
@@ -18,7 +22,7 @@ import android.view.SurfaceView;
 public class GameSurfaceView extends SurfaceView implements Runnable {
 
     // This is our thread
-    Thread gameThread = null;
+    private Thread mGameThread = null;
     private long mStartTime;
 
     private int mBackgroundColor;
@@ -26,17 +30,17 @@ public class GameSurfaceView extends SurfaceView implements Runnable {
 
     private float[] deltaRotationVector = new float[4];
 
-    SurfaceHolder ourHolder;
+    private String mText;
+
+    private SurfaceHolder mHolder;
 
     private Vibrator mVibrator;
 
-    // A boolean which we will set and unset
-    // when the game is running- or not.
     volatile boolean playing;
 
     // A Canvas and a Paint object
-    Canvas canvas;
-    Paint paint;
+    Canvas mCanvas;
+    Paint mPaint;
 
     // This variable tracks the game frame rate
     long fps;
@@ -56,9 +60,11 @@ public class GameSurfaceView extends SurfaceView implements Runnable {
 
     private void init(Context context) {
         // Initialize ourHolder and paint objects
-        ourHolder = getHolder();
-        paint = new Paint();
-        paint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        mHolder = getHolder();
+        mPaint = new Paint();
+        mPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        Typeface azoSans = Typeface.createFromAsset(getContext().getAssets(), "fonts/azo-sans-uber.ttf");
+        mPaint.setTypeface(azoSans);
 
         mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 
@@ -69,11 +75,11 @@ public class GameSurfaceView extends SurfaceView implements Runnable {
     }
 
     public void setRotationVector(float[] rotationVector) {
+//        Log.d("HSV", String.format("%f, %f, %f", deltaRotationVector[0], deltaRotationVector[1], deltaRotationVector[2]));
         deltaRotationVector = rotationVector;
     }
 
     private void update() {
-        Log.d("HSV", String.format("%f, %f, %f", deltaRotationVector[0], deltaRotationVector[1], deltaRotationVector[2]));
         if (deltaRotationVector[0] == 0) return;
         float[] hsv = new float[3];
         Color.colorToHSV(mCircleColor, hsv);
@@ -98,41 +104,63 @@ public class GameSurfaceView extends SurfaceView implements Runnable {
 
     private void draw() {
         // Make sure our drawing surface is valid or we crash
-        if (ourHolder.getSurface().isValid()) {
+        if (mHolder.getSurface().isValid()) {
             // Lock the canvas ready to draw
             // Make the drawing surface our canvas object
-            canvas = ourHolder.lockCanvas();
+            mCanvas = mHolder.lockCanvas();
 
             // Draw the background color
-            canvas.drawColor(mBackgroundColor);
+            mCanvas.drawColor(mBackgroundColor);
 
             // Choose the brush color for drawing
-            paint.setColor(mCircleColor);
+            mPaint.setColor(mCircleColor);
 
             // Draw the circle in the center (shrinking)
             long endTime = SystemClock.elapsedRealtime();
             long elapsedMilliSeconds = endTime - mStartTime + 1;
-            canvas.drawCircle(
-                    canvas.getWidth() / 2,
-                    canvas.getHeight() / 2,
-                    (canvas.getHeight() / 4) - elapsedMilliSeconds / 50,
-                    paint
+            mCanvas.drawCircle(
+                    mCanvas.getWidth() / 2,
+                    mCanvas.getHeight() / 2,
+                    (mCanvas.getHeight() / 4) - elapsedMilliSeconds / 50,
+                    mPaint
             );
 
-//            // Choose the brush color for drawing
-//            paint.setColor(Color.argb(255,  249, 129, 0));
-//
-//            // Make the text a bit bigger
-//            paint.setTextSize(45);
-//
-//            // Display the current fps on the screen
-//            canvas.drawText("FPS:" + fps, 20, 40, paint);
+            // Choose the brush color for drawing
+            mPaint.setColor(Color.argb(255,  249, 129, 0));
 
+            // Make the text a bit bigger
+            mPaint.setTextSize(45);
+
+            // Display the current fps on the screen
+            mCanvas.drawText("FPS:" + fps, 20, 40, mPaint);
+
+            // Choose the brush color for drawing
+            mPaint.setColor(Color.WHITE);
+
+            if (mText != null) {
+                // Make the text a bit bigger
+                mPaint.setTextSize(100);
+
+
+
+                int xPos = (mCanvas.getWidth() / 2);
+                int yPos = (int) ((mCanvas.getHeight() / 2) - ((mPaint.descent() + mPaint.ascent()) / 2));
+                //((textPaint.descent() + textPaint.ascent()) / 2) is the distance from the baseline to the center.
+
+                mCanvas.drawText(mText, xPos, yPos, mPaint);
+            }
 
             // Draw everything to the screen
             // and unlock the drawing surface
-            ourHolder.unlockCanvasAndPost(canvas);
+            mHolder.unlockCanvasAndPost(mCanvas);
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d("HSV", "onTouchEvent()");
+        mText = "Excellent!";
+        return super.onTouchEvent(event);
     }
 
     // If SimpleGameEngine Activity is paused/stopped
@@ -140,7 +168,7 @@ public class GameSurfaceView extends SurfaceView implements Runnable {
     public void pause() {
         playing = false;
         try {
-            gameThread.join();
+            mGameThread.join();
         } catch (InterruptedException e) {
             Log.e("Error:", "joining thread");
         }
@@ -150,8 +178,8 @@ public class GameSurfaceView extends SurfaceView implements Runnable {
     // start our thread.
     public void resume() {
         playing = true;
-        gameThread = new Thread(this);
-        gameThread.start();
+        mGameThread = new Thread(this);
+        mGameThread.start();
     }
 
     @Override
